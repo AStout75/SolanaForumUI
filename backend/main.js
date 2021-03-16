04e4f5a2261d4295add8cc33669238416d559efd
Leo Stone <leocstone@gmail.com>
2021-03-16 16:55:58 -0400

    remove console input prompt

--- backend/main.js
+++ backend/main.js
@@ -23,11 +23,6 @@
     var io;
     io = server.setUpServer();
     events.setUpSocketEvents(io);
-
-    console.log("--------------------Solana forum demo--------------------");
-    const readlineSync = require('readline-sync');
-    let options = ["View Posts", "New Post", "Like Post"];
-    let response = readlineSync.keyInSelect(options, "Choose one (New post assumes you have a valid store)")
     
     // Establish connection to the cluster
     await hw1.establishConnection();
@@ -38,61 +33,6 @@
     // Load the program if not already loaded
     await hw1.loadProgram();
   
-    switch(response) {
-      case 0:
-        // Get accounts owned by the program
-        // await reportAccounts();
-        await hw1.reportAccounts();
-        /*
-        let view = await getArrayOfPosts();
-        for (var val of view) {
-          console.log(val); 
-        }
-        */
-        break;
-      case 1:
-        
-        // Enter post text
-        let postBody = readlineSync.question('Enter your post: ', { hideEchoBack: false });
-  
-        // Say hello to an account
-        await hw1.sayHello(postBody, "post");
-  
-        // Find out how many times that account has been greeted
-        await hw1.reportHellos();
-  
-        // Get accounts owned by the program
-        // await reportAccounts();
-  
-        /*let postRet = await getArrayOfPosts();
-        for (var val of postRet) {
-          console.log(val);
-        }*/
-  
-        break;
-      /*
-      case 2:
-        let ret = await getArrayOfPosts();
-        for (var val of ret) {
-          console.log(val);
-        }
-        let choice = readlineSync.question('Enter which post number you would like to send a like to: ', { hideEchoBack: false });
-        var num: number = +choice;
-        var split = ret[num].split(" - ");
-        // console.log(split);
-        await sayHello(split[2], "like");
-  
-        // Find out how many times that account has been greeted
-        await reportHellos();
-  
-        // Get accounts owned by the program
-        let viewRet = await getArrayOfPosts();
-        console.log(viewRet);
-        break
-      */
-      default:
-        break;
-    }
     console.log('Success');
   }
   
